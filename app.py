88c3eb9722db596fd6d590ab1a536da2401c2b67
anandadagit <anandveeraswamy@gmail.com>
2024-05-24 14:32:41 +0100

    database works

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:password@localhost/students'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres@localhost/students'
 
 db=SQLAlchemy(app)
 
